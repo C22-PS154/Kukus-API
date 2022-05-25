dc6804ae6ff1c36ba04fa058287a6060babf88a0
Yovan Yudhistira <yovan.yudhistira@ui.ac.id>
2022-05-25 21:51:36 +0700

    mongodb working

--- index.js
+++ index.js
@@ -1,31 +1,93 @@
-//make sure to install express and firestore
-const { Firestore } = require('@google-cloud/firestore')
+//install mongoose, express, bcrypt, nanoid
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const {User, Bio} = require('./model')
+const bcrypt = require('bcrypt')
+const {nanoid} = require('nanoid')
 
 app.use(express.json())
 
-const firestore = new Firestore({
-    projectId: '',
-    keyFileName: ''
+//connect ke cloud db mongodb
+mongoose.connect('mongodb+srv://fauzansyawalino:Fauzan16!@clusterkukus.h1tj9.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
 })
 
-const db = firestore.collection('')
+//test connection ke db
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection error:"))
+db.once('open', ()=>{console.log("connection success")})
 
-app.post('/post', async (req,res)=>{
-    const nama = req.body.nama
+//register, akun dan biodata
+app.post('/register', async (req,res)=>{
+    const email = req.body.email
+    const usern = req.body.usern
+    const pass = req.body.pass
+    const id = nanoid(6)
 
-    await db.doc(`${nama}`).set({
-        name: nama
+    const hash = await bcrypt.hash(pass,2)
+    const example = new User({
+        email:email,
+        username:usern,
+        password:hash,
+        userid:id
     })
+
+    await example.save()
     res.send('success')
 })
 
-app.get('/',async (req,res)=>{
-    const result = await db.get() 
-    res.send(result.docs.map(doc => doc.data()))
+app.post('/regbio', async (req,res)=>{
+    const goal = req.body.goal
+    const gender = req.body.gender
+    const ttl = req.body.ttl
+    const height = req.body.height
+    const weight = req.body.weight
+    const act = req.body.goal
+    const speed = req.body.speed
+    const id = nanoid(6)
+
+    const example2 = new Bio({
+        tujuan_ideal:goal,
+        jenis_kelamin:gender,
+        ttl:ttl,
+        tinggi_badan:height,
+        berat_badan:weight,
+        tingkat_aktivitas:act,
+        kecepatan_ideal:speed,
+        userid: id
+    })
+
+    await example2.save()
+    res.send('success')
 })
 
-app.listen(8080,()=>{
-    console.log('Connected on port 8080')
+//login
+app.get('/login', async (req,res)=>{
+    const email = req.body.email
+    const pass = req.body.pass
+    const result = await User.find({email:`${email}`},{_id:0,password:1,email:1})
+    console.log(result)
+    const ex = await bcrypt.compare(pass, result[0].password)
+    console.log(ex)
+    if(ex==true){
+        res.send({email:result[0].email})
+        //res.redirect('/home') 
+    }else{
+        res.send('login fail')
+    }
+})
+
+
+/*app.get('/hash', async (req,res)=>{
+    const pass = req.body.pass
+    const passhash = '$2b$04$X1TMYsDeKXIcou/CLz4wkOC6SeTsuXODtLyThrIjibjCwyvZ3mM/S'
+    const ex = await bcrypt.compare(pass, passhash)
+    const hash = await bcrypt.hash(pass,2)
+    res.send(ex)
+})*/
+
+app.listen(8080, ()=>{
+    console.log('connect at 8080')
 })
---
+++ model.js
@@ -0,0 +1,30 @@
+//Require Mongoose
+const mongoose = require("mongoose");
+
+//Inisiasi Schema
+const Schema = mongoose.Schema;
+
+//Pembuatan Schema UserSchema
+const UserSchema = new Schema({
+  username: { type: String, required: true, },
+  email: { type: String, required: true, },
+  password: { type: String, required: true, },
+  userid: { type: String, required: true }
+});
+
+const BioSchema = new Schema({
+  tujuan_ideal: { type: String, enum: ['l', 'g'], required: true, },
+  jenis_kelamin: { type: String, enum: ['l', 'p'], required: true, },
+  ttl: { type: String, required: true, },
+  tinggi_badan: { type: Number, required: true, },
+  berat_badan: { type: Number, required: true, },
+  tingkat_aktivitas: { type: String, enum: ['menetap', 'sedikit_aktif', 'cukup_aktif', 'sangat_aktif', 'ekstra_aktif'], required: true, },
+  kecepatan_ideal: { type: String, enum: ['slow', 'normal', 'fast'], required: true, },
+  userid: { type: String, required: true }
+});
+
+//Pembuatan Model atas Schema UserSchema
+const User = mongoose.model("User", UserSchema, "Users");
+const Bio = mongoose.model("Bio", BioSchema, "Biodata");
+
+module.exports = {User, Bio};
